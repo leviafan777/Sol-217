b231e408304cf78ff48b9f628c5951e301287e18
leviafan777 <vladimirleviy2005@gmail.com>
2025-06-13 12:42:46 +0300

    Game

---
+++ Assets/Code/FlashlightToggle.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class FlashlightToggle : MonoBehaviour
+{
+    public GameObject spotlightObject; 
+
+    private bool isActive = false;
+
+    void Start()
+    {
+        if (spotlightObject == null)
+        {
+           
+            return;
+        }
+
+        spotlightObject.SetActive(isActive);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            isActive = !isActive;
+            spotlightObject.SetActive(isActive);
+        }
+    }
+}
